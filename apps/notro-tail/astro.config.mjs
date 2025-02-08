1fddc96bef79543873083ea0ce79059dd00391bd
mosugi <mosugix@gmail.com>
2025-02-08 12:14:43 +0900

    refactor: remove Tailwind integration and related dependencies

--- apps/notro-tail/astro.config.mjs
+++ apps/notro-tail/astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -12,5 +11,5 @@ export default defineConfig({
       },
     ],
   },
-  integrations: [tailwind(), sitemap()],
+  integrations: [sitemap()],
 });
